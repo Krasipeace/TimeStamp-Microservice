37fe013e8c728e5ef7412bc0b68152935403f039
Krasimir Dramaliev <krasipeace@abv.bg>
2024-04-29 16:12:28 +0300

    add input data validation in the app

--- TimestampMicroservice.App/wwwroot/js/site.js
+++ TimestampMicroservice.App/wwwroot/js/site.js
@@ -1,10 +1,10 @@
 ﻿function copyUnix() {
-    document.querySelector('.copy-btn').addEventListener('click', function() {
+    document.querySelector('.copy-btn').addEventListener('click', function () {
         var copyText = document.getElementById("unixInput");
         copyText.style.display = "block";
         copyText.select();
         navigator.clipboard.writeText(copyText.value)
-            .then(function() {
+            .then(function () {
                 copyText.style.display = "none";
                 var popup = document.createElement("div");
                 popup.innerHTML = "Copied the timestamp: " + copyText.value;
@@ -13,12 +13,33 @@
                 popup.style.justifyContent = "center";
                 popup.style.alignItems = "center";
                 document.body.appendChild(popup);
-                setTimeout(function() {
+                setTimeout(function () {
                     document.body.removeChild(popup);
                 }, 1000);
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 console.error("Failed to copy timestamp: " + error);
             });
     });
+}
+
+function validateTimestamp() {
+    var timestamp = document.getElementById('timestamp').value;
+    var unixTimestamp = parseInt(timestamp);
+
+    if (isNaN(unixTimestamp) || unixTimestamp < 0 || unixTimestamp > 253402300799) {
+        var popup = document.createElement("div");
+        popup.innerHTML = "Invalid timestamp. The timestamp must be a number between 0 and 253402300799.";
+        popup.classList.add("popup");
+        popup.style.display = "flex";
+        popup.style.justifyContent = "center";
+        popup.style.alignItems = "center";
+        document.body.appendChild(popup);
+        setTimeout(function () {
+            document.body.removeChild(popup);
+        }, 5000);
+        return false;
+    }
+
+    return true;
 }
--- TimestampMicroservice.Common/AppConstants.cs
+++ TimestampMicroservice.Common/AppConstants.cs
@@ -10,5 +10,8 @@
         public const string InvalidResponseExceptionMessage = "Cannot retrieve timestamp from the server.";
 
         public const string TimestampInputRegexValidation = "@[1-9]\\d{0,12}";
+
+        public const string GetCurrentViewTitle = "Get Unix Timestamp";
+        public const string GetDateTimeViewTitle = "Paste Timestamp and get Date and Time";
     }
 }
